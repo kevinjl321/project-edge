a3e631791b271f37cc88be34b562688fbd516db8
Kevin Lan <kevinlan321@gmail.com>
2020-11-01 22:48:30 -0800

    added text gradient

--- src/components/hero/TwoColumnWithVideo.js
+++ src/components/hero/TwoColumnWithVideo.js
@@ -15,16 +15,16 @@ import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/dot-pattern.svg";
 import DesignIllustration from "../../images/graduation.svg";
 
-const Container = tw.div`relative bg-gray-800 -mx-8 px-8 text-gray-100`;
+const Container = tw.div`relative bg-white -mx-8 px-8 text-gray-100`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row md:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
 const LeftColumn = tw.div`relative lg:w-6/12 lg:pr-12 flex-shrink-0 text-center lg:text-left`;
 const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
 
-const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl`;
+const Heading = tw.h1`font-black text-gray-800 text-3xl md:text-5xl leading-snug max-w-3xl`;
 const Paragraph = tw.p`my-5 lg:my-8 text-xl lg:text-xl font-medium text-gray-500 max-w-lg mx-auto lg:mx-0`;
 
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
-const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-gray-100 text-primary-600 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
+const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-gray-300 text-primary-600 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
 const SecondaryButton = tw.button`mt-3 mx-6 sm:mt-0 md:mt-0 lg:mt-0 xl:mt-0 font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
 const IllustrationContainer = tw.div`flex justify-center md:justify-end items-center relative max-w-3xl lg:max-w-none`;
 
@@ -53,15 +53,13 @@ export default ({
   heading = <>
   Changing education as we know it
   <wbr />
-  <span tw="text-primary-500"> one student at a time.</span>
+  <span className = "w ln tl tv tw"> one student at a time.</span>
 </>,
   description="Further your education. Learn real world skills. All for the low price of $0, no catches, no hidden fees ever.",
   primaryButtonText="Learn More",
   primaryButtonUrl="/about",
   secondaryButtonText = "Sign Up",
   secondaryButtonUrl = "https://docs.google.com/forms/d/e/1FAIpQLScllcWv5IQ8ftIH7-V5dnsqLDk_X2EE_SzL7yQIdhiAdubRYw/viewform?usp=send_form",
-  watchVideoButtonText="Watch Video",
-  watchVideoYoutubeUrl="https://www.youtube.com/embed/_GuOjXYl5ew",
   imageSrc=DesignIllustration,
   imageCss=null,
   imageDecoratorBlob = false,
@@ -95,20 +93,6 @@ export default ({
           </RightColumn>
         </TwoColumn>
         <DecoratorBlob1 />
-        <StyledModal
-          closeTimeoutMS={300}
-          className="mainHeroModal"
-          isOpen={modalIsOpen}
-          onRequestClose={toggleModal}
-          shouldCloseOnOverlayClick={true}
-        >
-          <CloseModalButton onClick={toggleModal}>
-            <CloseIcon tw="w-6 h-6" />
-          </CloseModalButton>
-          <div className="content">
-            <ResponsiveVideoEmbed url={watchVideoYoutubeUrl} tw="w-full" />
-          </div>
-        </StyledModal>
       </Container>
     </>
   );
