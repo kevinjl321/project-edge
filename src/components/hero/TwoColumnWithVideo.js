982608dbada8d6cdc3cb6577bd4ed8085a854219
Kevin Lan <kevinlan321@gmail.com>
2020-11-04 20:58:56 -0800

    changed hover colors of buttons

--- src/components/hero/TwoColumnWithVideo.js
+++ src/components/hero/TwoColumnWithVideo.js
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-//eslint-disable-next-line
-import { css } from "styled-components/macro";
 
 import Header from "../headers/light.js";
 
-import ReactModalAdapter from "../../helpers/ReactModalAdapter.js";
-import ResponsiveVideoEmbed from "../../helpers/ResponsiveVideoEmbed.js";
-
-import { ReactComponent as PlayIcon } from "feather-icons/dist/icons/play-circle.svg";
-import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/dot-pattern.svg";
 import DesignIllustration from "../../images/graduation.svg";
@@ -24,30 +17,16 @@ const Heading = tw.h1`font-black text-gray-800 text-3xl md:text-5xl leading-snug
 const Paragraph = tw.p`my-5 lg:my-8 text-xl lg:text-xl font-medium text-gray-500 max-w-lg mx-auto lg:mx-0`;
 
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
-const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-gray-300 text-primary-600 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
+const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-gray-300 text-primary-600 hocus:bg-primary-700 hocus:text-gray-100 focus:shadow-outline focus:outline-none transition duration-300`;
 const SecondaryButton = tw.button`mt-3 mx-6 sm:mt-0 md:mt-0 lg:mt-0 xl:mt-0 font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
 const IllustrationContainer = tw.div`flex justify-center md:justify-end items-center relative max-w-3xl lg:max-w-none`;
 
-// Random Decorator Blobs (shapes that you see in background)
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
   ${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3  -z-10`}
 `;
 const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
   ${tw`pointer-events-none fill-current text-primary-500 opacity-25 absolute w-32 h-32 right-0 bottom-0 transform translate-x-10 translate-y-10 -z-10`}
 `;
-
-const StyledModal = styled(ReactModalAdapter)`
-  &.mainHeroModal__overlay {
-    ${tw`fixed inset-0 z-50`}
-  }
-  &.mainHeroModal__content {
-    ${tw`xl:mx-auto m-4 sm:m-16 max-w-screen-xl absolute inset-0 flex justify-center items-center rounded-lg bg-gray-200 outline-none`}
-  }
-  .content {
-    ${tw`w-full lg:p-16`}
-  }
-`;
-const CloseModalButton = tw.button`absolute top-0 right-0 mt-8 mr-8 hocus:text-primary-500`;
 
 export default ({
   heading = <>
@@ -64,9 +43,6 @@ export default ({
   imageCss=null,
   imageDecoratorBlob = false,
 }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const toggleModal = () => setModalIsOpen(!modalIsOpen);
 
   return (
     <>
