c777f4fd6ed9a6cb72884c7b83d8db539307bb2b
Kevin Lan <kevinlan321@gmail.com>
2021-03-19 23:57:49 -0700

    temp remove emw logo to fix loading issue

--- src/components/sections/Sponsors.js
+++ src/components/sections/Sponsors.js
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import { ReactComponent as SvgDotPatternIcon } from "../../images/dot-pattern.svg";
 import { SectionHeading as HeadingTitle } from "../base/Headings.js";
 import EMWLogo from "images/EMWLogo.jpg"
 
-const Container = tw.div`relative`;
+const Container = tw.div`relative bg-gray-300 -mx-8 px-8 text-gray-800`;
 
 const SingleColumn = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 
@@ -15,12 +14,12 @@ const HeadingDescription = tw.p`mt-4 font-medium text-gray-600 text-center max-w
 const Content = tw.div`mt-16`;
 
 const Card = styled.div(props => [
-  tw`mt-24 md:flex justify-center items-center`,
+  tw`mt-8 md:flex justify-center items-start`,
   props.reversed ? tw`flex-row-reverse` : "flex-row"
 ]);
 const Image = styled.div(props => [
   `background-image: url("${props.imageSrc}");`,
-  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
+  tw`md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
 ]);
 const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
 const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
@@ -28,24 +27,10 @@ const Title = tw.h4`text-3xl font-bold text-gray-900`;
 const Description = tw.p`mt-2 text-sm leading-loose`;
 const Link = tw.a`inline-block mt-4 text-sm text-primary-500 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-primary-500`;
 
-const SvgDotPattern1 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 left-0 transform -translate-x-20 rotate-90 translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern2 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 right-0 transform translate-x-20 rotate-45 translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern3 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 left-0 transform -translate-x-20 rotate-45 -translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern4 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 right-0 transform translate-x-20 rotate-90 -translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-
 export default () => {
   const cards = [
     {
-      imageSrc: EMWLogo,
-      subtitle: "",
+      imageSrc: "",
       title: "East Meets West Parent Education Club",
       description:
         "A nonprofit community service organization located in San Marino, CA and serving the surrounding area.",
@@ -59,7 +44,7 @@ export default () => {
         <HeadingInfoContainer>
           <HeadingTitle>Sponsors & Partners</HeadingTitle>
           <HeadingDescription>
-            Here are some of the most popular events in New York City curated by professionals.
+            A special thanks to those listed below for their contributions towards the democratization of education!
           </HeadingDescription>
         </HeadingInfoContainer>
 
@@ -77,10 +62,6 @@ export default () => {
           ))}
         </Content>
       </SingleColumn>
-      <SvgDotPattern1 />
-      <SvgDotPattern2 />
-      <SvgDotPattern3 />
-      <SvgDotPattern4 />
     </Container>
   );
 };
